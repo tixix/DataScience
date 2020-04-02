fb992f94ff821f9bf0aea1097102556173da6791
xhoa <send2me2nd@gmail.com>
2020-04-02 16:15:17 +0200

    added new files and folder restructured

---
+++ Python/python_useful.py
@@ -0,0 +1,73 @@
+# Some Usefull Script
+
+
+# Fetch from an API
+
+import requests
+
+# Fetch the Hackernews post
+resp = requests.get("https://hacker-news.firebaseio.com/v0/item/16222426.json")
+
+# Print the response parsed as JSON
+print(resp.json())
+
+# Assign the score of the test to post_score
+post_score = resp.json()['score']
+print(post_score)
+
+# Read from a postgres database
+
+# Function to extract table to a pandas DataFrame
+def extract_table_to_pandas(tablename, db_engine):
+    query = "SELECT * FROM {}".format(tablename)
+    return pd.read_sql(query, db_engine)
+
+# Connect to the database using the connection URI
+connection_uri = "postgresql://repl:password@localhost:5432/pagila" 
+db_engine = sqlalchemy.create_engine(connection_uri)
+
+# Extract the film table into a pandas DataFrame
+extract_table_to_pandas('film', db_engine)
+
+# Extract the customer table into a pandas DataFrame
+extract_table_to_pandas('customer', db_engine)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################# DASK ###############
+## in the big data world Spark is probably a more popular choice for data processing.
+## use the PySpark package to handle a Spark DataFrame
+import dask.dataframe as dd
+
+# Set the number of pratitions
+athlete_events_dask = dd.from_pandas(athlete_events, npartitions = 4)
+
+# Calculate the mean Age per Year
+print(athlete_events_dask.groupby('Year').Age.mean().compute())
+
+
+############ PySpark ########
+
+
+# Print the type of athlete_events_spark
+print(type(athlete_events_spark))
+
+# Print the schema of athlete_events_spark
+print(athlete_events_spark.printSchema())
+
+# Group by the Year, and find the mean Age
+print(athlete_events_spark.groupBy('Year').mean('Age'))
+
+# Group by the Year, and find the mean Age
+print(athlete_events_spark.groupBy('Year').mean('Age').show())
